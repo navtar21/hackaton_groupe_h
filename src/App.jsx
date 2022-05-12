e62606ec8ab1977ec8857f739a1def45bed75f41
navtar21 <maxime.m21@gmail.com>
2022-05-12 22:44:50 +0200

    creation page video

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Page404 from "./pages/Page404";
+import Petite_maison from "./pages/Petite_maison";
 
 import "./css/App.scss";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<Page404 />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Page404 />} />
+        <Route path="/petite_maison" element={<Petite_maison />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
