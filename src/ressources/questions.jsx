7c20bf6462033d03342b23e8c9e4af44ba7d173d
Benoît Vandanjon <benoit@vandanjon.fr>
2022-05-12 13:40:25 +0200

    add JSON for questions

---
+++ src/ressources/questions.jsx
@@ -0,0 +1,32 @@
+export const questions = [
+    {
+        id: 1,
+        title: "Question 01",
+        text: "This is question 01",
+        choice: [
+            {
+                id: 1,
+                text: "bien",
+            },
+            {
+                id: 2,
+                text: "Back to start",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Question 02",
+        text: "This is question 02",
+        choice: [
+            {
+                id: 1,
+                text: "bien",
+            },
+            {
+                id: 2,
+                text: "pas bien",
+            },
+        ],
+    },
+];
