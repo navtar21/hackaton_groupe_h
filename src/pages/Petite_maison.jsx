949eaa058823fe6826bdcb18d936020e28220f7e
navtar21 <maxime.m21@gmail.com>
2022-05-13 09:49:54 +0200

    ajout bouton sous la video

--- src/pages/Petite_maison.jsx
+++ src/pages/Petite_maison.jsx
@@ -1,5 +1,14 @@
 import Video from "../components/Video";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 export default function Petite_maison() {
-    return <Video />;
+  return (
+    <>
+      <Video />
+      <Link to="/home">
+        <Button content="TRASH" />
+      </Link>
+    </>
+  );
 }
